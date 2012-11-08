6be1236679033d9d8343edc2f296cc64e2283521
Sebastian Goettschkes <sebastian.goettschkes@googlemail.com>
2012-11-08 21:54:25 +0100

    getting intl exentions running for php

--- misc/install.sh
+++ misc/install.sh
@@ -3,7 +3,7 @@
 OPENSHIFT_RUNTIME_DIR=$OPENSHIFT_HOMEDIR/app-root/runtime
 OPENSHIFT_REPO_DIR=$OPENSHIFT_HOMEDIR/app-root/runtime/repo
 
-# INSTALL
+# INSTALL APACHE
 cd $OPENSHIFT_RUNTIME_DIR
 mkdir srv
 mkdir srv/pcre
@@ -39,6 +39,17 @@ cd ../httpd-2.4.3
 --enable-headers
 make && make install
 cd ..
+rm -r $OPENSHIFT_RUNTIME_DIR/tmp/*.tar.gz
+
+# INSTALL PHP
+wget http://download.icu-project.org/files/icu4c/50.1/icu4c-50_1-src.tgz
+tar -zxf icu4c-50_1-src.tgz
+cd icu/source/
+chmod +x runConfigureICU configure install-sh
+./configure \
+--prefix=$OPENSHIFT_RUNTIME_DIR/srv/icu/
+make && make install
+cd ../..
 wget http://de2.php.net/get/php-5.4.7.tar.gz/from/this/mirror
 tar -zxf php-5.4.7.tar.gz
 cd php-5.4.7
@@ -48,7 +59,9 @@ cd php-5.4.7
 --with-config-file-path=$OPENSHIFT_RUNTIME_DIR/srv/php/etc/apache2 \
 --with-layout=PHP \
 --with-apxs2=$OPENSHIFT_RUNTIME_DIR/srv/httpd/bin/apxs \
---enable-mbstring
+--enable-mbstring \
+--enable-intl \
+--with-icu-dir=$OPENSHIFT_RUNTIME_DIR/srv/icu
 make && make install
 mkdir $OPENSHIFT_RUNTIME_DIR/srv/php/etc/apache2
 cd ..
@@ -59,7 +72,7 @@ $OPENSHIFT_RUNTIME_DIR/srv/php/bin/phpize
 ./config \
 --with-php-config=$OPENSHIFT_RUNTIME_DIR/srv/php/bin/php-config \
 --enable-apc \
---enable-apc-debug
+--enable-apc-debug=no
 make && make install
 
 # CLEANUP
